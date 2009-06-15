af0dd790ef17b88f57290d60024cd8fd44c978cc
David Bruce <davidstuartbruce @nospam@ gmail.com>
2009-06-15 19:24:21 +0000

    adjust indents to match rest of tuxmath
    git-svn-id: svn://svn.debian.org/svn/tux4kids/tuxmath/branches/lan@1052 47d30e19-490b-0410-9d18-e851f4e465b3

--- server/server.c
+++ server/server.c
@@ -29,198 +29,183 @@ TCPsocket sd, csd; /* Socket descriptor, Client socket descriptor */
 
 int main(int argc, char **argv)
 { 
-       
-        IPaddress ip, *remoteIP;
-        int quit, quit2;
-        char buffer[512];
-        int network_function=-1;
-        MC_FlashCard* fc;
-   //     size_t length;
+  IPaddress ip, *remoteIP;
+  int quit, quit2;
+  char buffer[512];
+  int network_function = -1;
+  MC_FlashCard* fc;
+  //     size_t length;
 
         
-        if (SDLNet_Init() < 0)
-        {
-                fprintf(stderr, "SDLNet_Init: %s\n", SDLNet_GetError());
-                exit(EXIT_FAILURE);
-        }
+  if (SDLNet_Init() < 0)
+  {
+    fprintf(stderr, "SDLNet_Init: %s\n", SDLNet_GetError());
+    exit(EXIT_FAILURE);
+  }
  
-        /* Resolving the host using NULL make network interface to listen */
-        if (SDLNet_ResolveHost(&ip, NULL, 4778) < 0)
-        {
-                fprintf(stderr, "SDLNet_ResolveHost: %s\n", SDLNet_GetError());
-                exit(EXIT_FAILURE);
-        }
+  /* Resolving the host using NULL make network interface to listen */
+  if (SDLNet_ResolveHost(&ip, NULL, 4778) < 0)
+  {
+    fprintf(stderr, "SDLNet_ResolveHost: %s\n", SDLNet_GetError());
+    exit(EXIT_FAILURE);
+  }
  
-        /* Open a connection with the IP provided (listen on the host's port) */
-        if (!(sd = SDLNet_TCP_Open(&ip)))
-        {
-                fprintf(stderr, "SDLNet_TCP_Open: %s\n", SDLNet_GetError());
-                exit(EXIT_FAILURE);
-        }
+  /* Open a connection with the IP provided (listen on the host's port) */
+  if (!(sd = SDLNet_TCP_Open(&ip)))
+  {
+    fprintf(stderr, "SDLNet_TCP_Open: %s\n", SDLNet_GetError());
+    exit(EXIT_FAILURE);
+  }
  
-        /* Wait for a connection, send data and term */
-        quit = 0;
-        while (!quit)
-        {
-                /* This check the sd if there is a pending connection.
-                * If there is one, accept that, and open a new socket for communicating */
-                if ((csd = SDLNet_TCP_Accept(sd)))
-                {
-                        /* Now we can communicate with the client using csd socket
-                        * sd will remain opened waiting other connections */
+  /* Wait for a connection, send data and term */
+  quit = 0;
+  while (!quit)
+  {
+    /* This check the sd if there is a pending connection.
+     * If there is one, accept that, and open a new socket for communicating */
+    if ((csd = SDLNet_TCP_Accept(sd)))
+    {
+      /* Now we can communicate with the client using csd socket
+       * sd will remain opened waiting other connections */
  
-                        /* Get the remote address */
-                        if ((remoteIP = SDLNet_TCP_GetPeerAddress(csd)))
-                                /* Print the address, converting in the host format */
-                                printf("Host connected: %x %d\n", SDLNet_Read32(&remoteIP->host), SDLNet_Read16(&remoteIP->port));
-                        else
-                                fprintf(stderr, "SDLNet_TCP_GetPeerAddress: %s\n", SDLNet_GetError());
+      /* Get the remote address */
+      if ((remoteIP = SDLNet_TCP_GetPeerAddress(csd)))
+        /* Print the address, converting in the host format */
+        printf("Host connected: %x %d\n",
+	       SDLNet_Read32(&remoteIP->host),
+	       SDLNet_Read16(&remoteIP->port));
+      else
+        fprintf(stderr, "SDLNet_TCP_GetPeerAddress: %s\n", SDLNet_GetError());
                         
-                        quit2 = 0;
-                        while (!quit2)
-                        {
-                                
-                                if (SDLNet_TCP_Recv(csd, buffer, 512) > 0)
-                                {
-                                        network_function = -1;
-                                        printf("Client say: %s\n", buffer);
-                                        
-                                        //'a' for the setting up the question list                                           
-                                        if(strcmp(buffer,"a")==0)
-                                        {
-                                           network_function=SETUP_QUESTION_LIST;              
-					} 
+      quit2 = 0;
+      while (!quit2)
+      {
+        if (SDLNet_TCP_Recv(csd, buffer, 512) > 0)
+        {
+          network_function = -1;
+          printf("Client say: %s\n", buffer);
                                        
-					//'b' for asking for a question(flashcard)
-                                        if(strcmp(buffer,"b")==0)
-                                        {
-                                           network_function=SEND_A_QUESTION;              
-					} 
+          //'a' for the setting up the question list                                           
+          if(strcmp(buffer,"a") == 0)
+          {
+            network_function = SETUP_QUESTION_LIST;              
+          } 
+                                       
+          //'b' for asking for a question(flashcard)
+          if(strcmp(buffer,"b") == 0)
+          {
+            network_function = SEND_A_QUESTION;              
+          } 
  
-					switch(network_function)
- 
-					{
-						case SETUP_QUESTION_LIST:                                                //mainly to setup the question list
+          switch(network_function)
+          {
+            case SETUP_QUESTION_LIST:  //mainly to setup the question list
+            {
+              if (!MC_StartGame())
+              {
+                fprintf(stderr, "\nMC_StartGame() failed!");
+                return 0;
+              }                                                                                  
                                                  
-                                                 {
-                                                   if (!MC_StartGame())
-						   {
-						    
-						     fprintf(stderr, "\nMC_StartGame() failed!");
-						     return 0;
-						   }                                                                                  
-                                                 
-                                                   break;                                           
-                                                 } 
-						
-                                                case SEND_A_QUESTION:
-                                                  
-                                                  {
+              break;                                           
+            } 
 
-                                                   fc = (MC_FlashCard *)malloc(sizeof(MC_FlashCard));
-        
-                                                    if (fc == NULL) 
-            						{
-						             printf("Allocation of comets failed");
-						             return 0;
-					                }
-         
-					           else 
-					                {
-    						            *fc = MC_AllocateFlashcard();
-						            if (!MC_FlashCardGood(fc) ) 
-						             {
-						              //something's wrong
-						              printf("Allocation of flashcard failed\n");
-						              MC_FreeFlashcard(fc);
-						              return 0;
-						             }
-					                }
-					    //      fc->answer_string="";
-					    //      fc->formula_string="";
- 
-						    
-                                                    if (!MC_NextQuestion(fc))
-                                                    { 
-                                                      /* no more questions available - cannot create comet.  */
-                                                      return 0;
-                                                    }
-                                                   
-						    printf("WILL SEND >>\n");  
-                                                    
-						    printf("QUESTION_ID       :      %d\n",fc->question_id);
-                                                    printf("FORMULA_STRING    :      %s\n",fc->formula_string);
-                                                    printf("ANSWER STRING     :      %s\n",fc->answer_string);
-						    printf("ANSWER            :      %d\n",fc->answer);
-  						    printf("DIFFICULTY        :      %d\n",fc->difficulty);
-
-
-
-                                                    if(!SendQuestion(fc))
-                                                    {
-      				                       printf("Unable to send Question\n");
-     			                            }
-                                                  
-                                                    break;
-                                                  }	
-                                                  
-        
-						  default:
-                                                  break;				
-					}
-
-
-
-                                       if(strcmp(buffer, "exit") == 0) /* Terminate this connection */
-                                        {
-                                                quit2 = 1;
-                                                printf("Terminate connection\n");
-                                        }
-                                        if(strcmp(buffer, "quit") == 0) /* Quit the program */
-                                        {
-                                                quit2 = 1;
-                                                quit = 1;
-                                                printf("Quit program\n");
-                                        }
-                                }
-                        }
- 
-                        /* Close the client socket */
-                        SDLNet_TCP_Close(csd);
+            case SEND_A_QUESTION:
+            {
+              fc = (MC_FlashCard *)malloc(sizeof(MC_FlashCard));
+              if (fc == NULL) 
+              {
+                printf("Allocation of comets failed");
+                return 0;
+              }
+              else 
+              {
+                *fc = MC_AllocateFlashcard();
+                if (!MC_FlashCardGood(fc) ) 
+                {
+                  //something's wrong
+                  printf("Allocation of flashcard failed\n");
+                  MC_FreeFlashcard(fc);
+                  return 0;
                 }
+              }
+              //      fc->answer_string="";
+              //      fc->formula_string="";
+ 
+              if (!MC_NextQuestion(fc))
+              { 
+                /* no more questions available - cannot create comet.  */
+                return 0;
+              }
+                                                   
+              printf("WILL SEND >>\n");  
+              printf("QUESTION_ID       :      %d\n", fc->question_id);
+              printf("FORMULA_STRING    :      %s\n", fc->formula_string);
+              printf("ANSWER STRING     :      %s\n", fc->answer_string);
+              printf("ANSWER            :      %d\n",fc->answer);
+              printf("DIFFICULTY        :      %d\n",fc->difficulty);
+
+              if(!SendQuestion(fc))
+              {
+                printf("Unable to send Question\n");
+              }
+                                                 
+              break;
+            }                             
+        
+            default:
+              break;
+          }
+
+          if(strcmp(buffer, "exit") == 0) /* Terminate this connection */
+          {
+            quit2 = 1;
+            printf("Terminate connection\n");
+          }
+
+          if(strcmp(buffer, "quit") == 0) /* Quit the program */
+          {
+            quit2 = 1;
+            quit = 1;
+            printf("Quit program\n");
+          }
         }
+      }
  
-        SDLNet_TCP_Close(sd);
-        SDLNet_Quit();
+      /* Close the client socket */
+      SDLNet_TCP_Close(csd);
+    }
+  }
  
-        return EXIT_SUCCESS;
+  SDLNet_TCP_Close(sd);
+  SDLNet_Quit();
+ 
+  return EXIT_SUCCESS;
 }
 
 
-
-
-int SendQuestion(MC_FlashCard* fc)                           //function to send a flashcard(question) from the server to the client
+//function to send a flashcard(question) from the server to the client
+int SendQuestion(MC_FlashCard* fc)
 {
-      char *ch;
-      int x;
+  char *ch;
+  int x;
 
-      x=SDLNet_TCP_Send(csd,&(fc->question_id),sizeof(fc->question_id));
-      printf("no:(1):::QUESTION_ID::::Sent %d bytes\n",x);
+  x = SDLNet_TCP_Send(csd, &(fc->question_id), sizeof(fc->question_id));
+  printf("no:(1):::QUESTION_ID::::Sent %d bytes\n", x);
       
-      x=SDLNet_TCP_Send(csd,&(fc->difficulty),sizeof(fc->difficulty));
-      printf("no:(2):::DIFFICULTY::::Sent %d bytes\n",x);
+  x = SDLNet_TCP_Send(csd,&(fc->difficulty),sizeof(fc->difficulty));
+  printf("no:(2):::DIFFICULTY::::Sent %d bytes\n", x);
 
-      x=SDLNet_TCP_Send(csd,&(fc->answer),sizeof(fc->answer));
-      printf("no:(3)::::ANSWER:::Sent %d bytes\n",x);
+  x = SDLNet_TCP_Send(csd, &(fc->answer), sizeof(fc->answer));
+  printf("no:(3)::::ANSWER:::Sent %d bytes\n",x);
 
-      x=SDLNet_TCP_Send(csd,fc->answer_string,strlen(fc->answer_string)+1);
-      printf("no:(4):::ANSWER_STRING::::Sent %d bytes\n",x);
+  x = SDLNet_TCP_Send(csd, fc->answer_string, strlen(fc->answer_string) + 1);
+  printf("no:(4):::ANSWER_STRING::::Sent %d bytes\n", x);
 
-      x=SDLNet_TCP_Send(csd,fc->formula_string,strlen(fc->formula_string)+1);
-      printf("no:(5):::FORMULA_STRING::::Sent %d bytes\n",x);
+  x = SDLNet_TCP_Send(csd, fc->formula_string, strlen(fc->formula_string) + 1);
+  printf("no:(5):::FORMULA_STRING::::Sent %d bytes\n", x);
     
-      return 1;
-
+  return 1;
 }
 
 
--- server/testclient.c
+++ server/testclient.c
@@ -29,120 +29,124 @@ TCPsocket sd;           /* Socket descriptor */
 
 int main(int argc, char **argv)
 {
-        IPaddress ip;           /* Server address */
-        int quit, len;
-        char buffer[512];
-        MC_FlashCard* fc;
+  IPaddress ip;           /* Server address */
+  int quit, len;
+  char buffer[512];
+  MC_FlashCard* fc;
       
-        /* Simple parameter checking */
-        if (argc < 3)
-        {
-                fprintf(stderr, "Usage: %s host port\n", argv[0]);
-                exit(EXIT_FAILURE);
-        }
+  /* Simple parameter checking */
+  if (argc < 3)
+  {
+    fprintf(stderr, "Usage: %s host port\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
  
-        if (SDLNet_Init() < 0)
-        {
-                fprintf(stderr, "SDLNet_Init: %s\n", SDLNet_GetError());
-                exit(EXIT_FAILURE);
-        }
+  if (SDLNet_Init() < 0)
+  {
+    fprintf(stderr, "SDLNet_Init: %s\n", SDLNet_GetError());
+    exit(EXIT_FAILURE);
+  }
  
-        /* Resolve the host we are connecting to */
-        if (SDLNet_ResolveHost(&ip, argv[1], atoi(argv[2])) < 0)
-        {
-                fprintf(stderr, "SDLNet_ResolveHost: %s\n", SDLNet_GetError());
-                exit(EXIT_FAILURE);
-        }
+  /* Resolve the host we are connecting to */
+  if (SDLNet_ResolveHost(&ip, argv[1], atoi(argv[2])) < 0)
+  {
+    fprintf(stderr, "SDLNet_ResolveHost: %s\n", SDLNet_GetError());
+    exit(EXIT_FAILURE);
+  }
  
-        /* Open a connection with the IP provided (listen on the host's port) */
-        if (!(sd = SDLNet_TCP_Open(&ip)))
-        {
-                fprintf(stderr, "SDLNet_TCP_Open: %s\n", SDLNet_GetError());
-                exit(EXIT_FAILURE);
-        }
+  /* Open a connection with the IP provided (listen on the host's port) */
+  if (!(sd = SDLNet_TCP_Open(&ip)))
+  {
+    fprintf(stderr, "SDLNet_TCP_Open: %s\n", SDLNet_GetError());
+    exit(EXIT_FAILURE);
+  }
  
-        /* Send messages */
-        quit = 0;
-        while (!quit)
-        {
-                printf("Write something:\n>");
-                scanf("%s", buffer);
+  /* Send messages */
+  quit = 0;
+  while (!quit)
+  {
+    printf("Write something:\n>");
+    scanf("%s", buffer);
  
-                len = strlen(buffer) + 1;
-                if (SDLNet_TCP_Send(sd, (void *)buffer, len) < len)
-                {
-                        fprintf(stderr, "SDLNet_TCP_Send: %s\n", SDLNet_GetError());
-                        exit(EXIT_FAILURE);
-                }
-                if(strcmp(buffer,"b")==0)
-		{ 
-                  fc = (MC_FlashCard *)malloc(sizeof(MC_FlashCard));
-        
-                                                    if (fc == NULL) 
-            						{
-						             printf("Allocation of comets failed");
-						             return 0;
-					                }
-         
-					           else 
-					                {
-    						            *fc = MC_AllocateFlashcard();
-						            if (!MC_FlashCardGood(fc) ) 
-						             {
-						              //something's wrong
-						              printf("Allocation of flashcard failed\n");
-						              MC_FreeFlashcard(fc);
-						              return 0;
-						             }
-					                }
-                 if(!RecvQuestion(fc))
-                 printf("unable to recv question\n"); 
-                }  
+    len = strlen(buffer) + 1;
+    if (SDLNet_TCP_Send(sd, (void *)buffer, len) < len)
+    {
+      fprintf(stderr, "SDLNet_TCP_Send: %s\n", SDLNet_GetError());
+      exit(EXIT_FAILURE);
+    }
 
-                if(strcmp(buffer, "exit") == 0)
-                        quit = 1;
-                if(strcmp(buffer, "quit") == 0)
-                        quit = 1;
+    if(strcmp(buffer,"b") == 0)
+    { 
+      fc = (MC_FlashCard *)malloc(sizeof(MC_FlashCard));
+        
+      if (fc == NULL) 
+      {
+        printf("Allocation of comets failed");
+        return 0;
+      }
+      else 
+      {
+        *fc = MC_AllocateFlashcard();
+        if (!MC_FlashCardGood(fc) ) 
+        {
+          //something's wrong
+          printf("Allocation of flashcard failed\n");
+          MC_FreeFlashcard(fc);
+          return 0;
         }
+      }
+
+      if(!RecvQuestion(fc))
+        printf("unable to recv question\n"); 
+    }  
+
+    if(strcmp(buffer, "exit") == 0)
+      quit = 1;
+    if(strcmp(buffer, "quit") == 0)
+      quit = 1;
+  }
  
-        SDLNet_TCP_Close(sd);
-        SDLNet_Quit();
+  SDLNet_TCP_Close(sd);
+  SDLNet_Quit();
  
-        return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
 
-int RecvQuestion(MC_FlashCard* fc)                           //function to receive a flashcard(question) by the client
+
+//function to receive a flashcard(question) by the client
+int RecvQuestion(MC_FlashCard* fc)
 {
-       char ch[5];
-       int x,i=0;
+  char ch[5];
+  int x,i=0;
 
-       x=SDLNet_TCP_Recv(sd,&(fc->question_id),sizeof(fc->question_id));
-       printf("no:(1):::QUESTION_ID::::Received %d bytes\n",x);
+  x = SDLNet_TCP_Recv(sd, &(fc->question_id), sizeof(fc->question_id));
+  printf("no:(1):::QUESTION_ID::::Received %d bytes\n",x);
  
-       x=SDLNet_TCP_Recv(sd,&(fc->difficulty),sizeof(fc->difficulty));
-       printf("no:(2):::DIFFICULTY::::Received %d bytes\n",x);
+  x = SDLNet_TCP_Recv(sd, &(fc->difficulty), sizeof(fc->difficulty));
+  printf("no:(2):::DIFFICULTY::::Received %d bytes\n",x);
  
-       x=SDLNet_TCP_Recv(sd,&(fc->answer),sizeof(fc->answer));
-       printf("no:(3):::ANSWER::::Received %d bytes\n",x);
+  x = SDLNet_TCP_Recv(sd, &(fc->answer), sizeof(fc->answer));
+  printf("no:(3):::ANSWER::::Received %d bytes\n",x);
 
-       do{
-       x=SDLNet_TCP_Recv(sd,&ch[i],1);      
-       printf("<<<SUB-PACKET%d>>>no:(4):::ANSWER_STRING::::Received %d bytes\n",i,x);
-       i++;
-       }while(ch[i-1]!='\0');
-       strncpy(fc->answer_string,ch,i+1);
+  do{
+    x = SDLNet_TCP_Recv(sd, &ch[i], 1);      
+    printf("<<<SUB-PACKET%d>>>no:(4):::ANSWER_STRING::::Received %d bytes\n", i, x);
+    i++;
+  }while(ch[i-1]!='\0');
 
-       x=SDLNet_TCP_Recv(sd,fc->formula_string,13);
-       printf("no:(5):::FORMULA_STRING::::Received %d bytes\n",x);
+  strncpy(fc->answer_string, ch, i + 1);
+
+  x = SDLNet_TCP_Recv(sd, fc->formula_string, 13);
+
+  printf("no:(5):::FORMULA_STRING::::Received %d bytes\n",x);
+  printf("RECEIVED >>\n");
+  printf("QUESTION_ID    >>          %d\n",fc->question_id);  
+  printf("FORMULA_STRING >>          %s\n",fc->formula_string);  
+  printf("ANSWER_STRING  >>          %s\n",fc->answer_string);  
+  printf("ANSWER         >>          %d\n",fc->answer);  
+  printf("DIFFICULTY     >>          %d\n",fc->difficulty);  
        
-       printf("RECEIVED >>\n");
-       printf("QUESTION_ID    >>          %d\n",fc->question_id);  
-       printf("FORMULA_STRING >>          %s\n",fc->formula_string);  
-       printf("ANSWER_STRING  >>          %s\n",fc->answer_string);  
-       printf("ANSWER         >>          %d\n",fc->answer);  
-       printf("DIFFICULTY     >>          %d\n",fc->difficulty);  
-       
-       return 1;
+  return 1;
 }
 
 
