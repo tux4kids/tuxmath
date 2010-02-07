7aba5ba07689a4d37f1d64150a1d987bae2f8167
David Bruce <davidstuartbruce@gmail.com>
2010-02-07 12:24:11 -0600

    Fixing crossbuild scripts for Windows build

---
+++ buildw32/cross-make.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Script for crossbuilding of Windows binaries on Linux/Unix host
+# using mingw port of GCC. Obtained from http://www.libsdl.org
+# and lightly modified by David Bruce <davidstuartbruce@gmail.com>
+
+# Path to crossbuild setup:
+PREFIX=/usr/local/mingw
+
+# Architecture of the build machine's crossbuild setup
+# as located by PREFIX:
+TARGET=i586-pc-mingw32
+
+PATH="$PREFIX/bin:$PREFIX/$TARGET/bin:$PATH"
+export PATH
+exec make $*
