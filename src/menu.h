bf74f2593d12e6ad1c964e10a3ae1a3db5ded4be
David Bruce <davidstuartbruce@gmail.com>
2010-08-21 09:42:50 -0500

    More GPLv3+ notices

--- src/menu.h
+++ src/menu.h
@@ -1,16 +1,36 @@
 /*
-  menu.h
+   menu.h:
 
-  Functions responsible for loading, parsing and displaying game menu.
-  (interface)
+   Functions responsible for loading, parsing and displaying game menu.
+   (interface)
+ 
+   Copyright 2009, 2010.
+   Authors: Boleslaw Kulbabinski <bkulbabinski@gmail.com>
+            Brendan Luchen
+	    David Bruce
+   Project email: <tuxmath-devel@lists.sourceforge.net>
+   Project website: http://tux4kids.alioth.debian.org
 
-  Part of "Tux4Kids" Project
-  http://www.tux4kids.com/
 
-  Author: Boleslaw Kulbabinski <bkulbabinski@gmail.com>, (C) 2009
+menu.h is part of "Tux, of Math Command", a.k.a. "tuxmath".
 
-  Copyright: See COPYING file that comes with this distribution.
+Tuxmath is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+Tuxmath is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 */
+
+
+
 
 #ifndef MENU_H
 #define MENU_H
--- src/network.h
+++ src/network.h
@@ -1,12 +1,29 @@
 /*
+   network.h:
 
-        network.h
+   Provides routines for various networking functions to be used
+   in the LAN multiplayer game.
+ 
+   Copyright 2009, 2010.
+   Authors: David Bruce, Akash Gangil
+   Project email: <tuxmath-devel@lists.sourceforge.net>
+   Project website: http://tux4kids.alioth.debian.org
 
-        Description: Provides routines for various networking functions to be used
-                     in the LAN multiplayer game.
-        Author: David Bruce ,Akash Gangil and the TuxMath team, (C) 2009
 
-        Copyright: See COPYING file that comes with this distribution (briefly, GNU GPL version 2 or later)
+   network.h is part of "Tux, of Math Command", a.k.a. "tuxmath".
+
+Tuxmath is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+Tuxmath is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
