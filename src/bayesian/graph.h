660445e5e9fdd5b596b348a2162335db6480811a
Siddharth <sids.aquarius@gmail.com>
2011-07-20 18:36:44 +0530

    Year update in copyright notice

--- src/bayesian/graph.h
+++ src/bayesian/graph.h
@@ -3,7 +3,7 @@
    'Edge' and 'Graph'. The interface is independent from the
    representation used in implementation.
 
-   Copyright 2007, 2008, 2009, 2010.
+   Copyright 2011.
 Authors: Siddharth Kothari
 Project email: <tuxmath-devel@lists.sourceforge.net>
 Project website: http://tux4kids.alioth.debian.org
